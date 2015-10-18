6333de7352429bb39cb646eb642d5df53b37388a
ankurasthana <ankur@join25.org>
2015-10-18 01:29:51 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -124,3 +124,13 @@ return $count;
 }
 }
 
+//trimming post length on homepage
+function new_excerpt_more( $more ) {
+	return '...';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
+
+function custom_excerpt_length( $length ) {
+	return 16;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
