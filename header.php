9d3a733d88589ce32c39f4ba853cc42902a1c9c1
ankurasthana <ankur@join25.org>
2016-02-21 14:44:44 -0500

    Added files via upload

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://act.zerotbdeaths.org/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href="http://act.zerotbdeaths.org/wp-content/uploads/2015/10/faviconztb1.ico" rel="shortcut icon" type="image/x-icon" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</head>
+<nav class="navbar navbar-default" id="navbar-default-overrides">
+	<div class="container-fluid col-md-6 col-md-offset-1">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" id="navbar-toggle-overrides" data-toggle="collapse" data-target="#0tbd-header-navbar-collapse" aria-expanded="false">
+        			<span class="sr-only">Toggle navigation</span>
+       	 			<span class="icon-bar"></span>
+        			<span class="icon-bar"></span>
+        			<span class="icon-bar"></span>
+      			</button>
+      			<a class="navbar-brand" id="navbar-brand-overrides" href="http://act.zerotbdeaths.org/">
+				<img class="nav-logo" src="http://act.zerotbdeaths.org/wp-content/uploads/2015/10/logo-a8161ba7c4222f3bfb2bb8d03a118197-e1444495928747.png">
+			</a>
+	</div>
+	<div class="collapse navbar-collapse" id="0tbd-header-navbar-collapse">
+		<ul class="nav navbar-nav">
+			<li><a href="http://act.zerotbdeaths.org/?page_id=2"><i class="fa fa-pencil-square-o"></i>  Start a Petition</a></li>
+			<li><a href="http://act.zerotbdeaths.org/browse-petitions/"><i class="fa fa-search"></i>  Browse Petitions</a></li>
+		</ul>
+	</div>
+</div>
+</nav>
+
+
+</html>
