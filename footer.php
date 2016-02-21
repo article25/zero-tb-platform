06a4b1a065134df54b69b39fec4004367f4ee225
ankurasthana <ankur@join25.org>
2016-02-21 14:45:07 -0500

    Added files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<div class="clear"></div>
+</div>
+<footer id="footer" role="contentinfo">
+<div id="copyright">
+<div class="column"><div class="row"><div class="col-md-12 text-center">
+<?php echo sprintf( __( 'This site is managed by <a href="http://join25.org/">Article 25</a> | <a href="http://join25.org/terms-service/">Terms of Service</a> | <a href="http://join25.org/privacy-policy/">Privacy Policy</a>', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' ', 'blankslate' ), '' ); ?>
+</div></div></div>
+</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
